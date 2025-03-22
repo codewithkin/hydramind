d2508e3e7f2d6e474c073986164b3ac340529a07
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-22 13:21:34 +0200

    [ADD]: Dummy response to weather data controller

--- server/src/controllers/data/getWeather.ts
+++ server/src/controllers/data/getWeather.ts
@@ -2,7 +2,10 @@ import { Request, Response } from "express";
 
 export async function getWeather (req: Request, res: Response) {
     try {
-
+        // Return dummy weather data
+        res.status(200).json({
+            message: "Weather data here"
+        })
     } catch (e) {
         console.log("An error occured whike fetching weather data: ", e);
 
