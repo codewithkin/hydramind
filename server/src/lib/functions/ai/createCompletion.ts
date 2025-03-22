34774fe01a68860c268206ad2ce49d821ca70c52
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-22 14:51:52 +0200

    [ADD]: Create completions function
    Code format

---
+++ server/src/lib/functions/ai/createCompletion.ts
@@ -0,0 +1,24 @@
+import { client } from "./client.js";
+
+export async function createCompletion({ message }: { message: string }) {
+  try {
+    const completion = await client.chat.completions.create({
+      temperature: 0.6,
+      model: "meta-llama/Meta-Llama-3.1-70B-Instruct",
+      messages: [
+        {
+          role: "user",
+          content: message,
+        },
+      ],
+    });
+    
+    const response = completion.choices[0].message.content;
+
+    return response;
+  } catch (e) {
+    console.log("An error occured while creating completion: ", e);
+  }
+}
+
+createCompletion({message: "Hi there"})
