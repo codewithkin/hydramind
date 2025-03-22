68cd68f8f5c978525e9790eb2bb7087884578b97
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-22 17:17:47 +0200

    [ADD]: Auth controllers

---
+++ server/src/controllers/auth/authController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import User from "../../model/user";
+
+const register = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are Required!" });
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User alredy exists!" });
+    }
+
+    const user = await User.create({ name, email, password });
+    await user.save();
+    res
+      .status(201)
+      .json({ message: "User registered succesfully", user: user });
+  } catch (error) {
+    console.log("Error :", error);
+    res.status(500).json({ message: "User regitstraion Failed!" });
+  }
+};
+
+export default register;
