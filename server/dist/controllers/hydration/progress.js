9931eb26160bac3e5696a79c5c7e911be7dae29f
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-22 16:35:35 +0200

    Minor changes

---
+++ server/dist/controllers/hydration/progress.js
@@ -0,0 +1,18 @@
+export async function progress(req, res) {
+  try {
+    // Get the userId from the request body
+    const { userId } = req.query;
+    if (!userId) {
+      console.log("userId is missing, please provide it");
+    }
+    // Return dummy response
+    res.status(200).json({
+      message: "Yes, you made a request to the log progress endpoint",
+    });
+  } catch (e) {
+    console.log("An error occured while getting hydration progress data ", e);
+    res
+      .status(500)
+      .send("An error occured while getting hydration progress data");
+  }
+}
---
+++ server/dist/routes/notifications/index.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { sendNotification } from "../../controllers/notifications/sendNotification.js";
+const notificationRoutes = Router();
+// Send notification
+notificationRoutes.post("/send", sendNotification);
+export default notificationRoutes;
