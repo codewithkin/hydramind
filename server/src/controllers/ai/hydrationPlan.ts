2cce76b80ecb48d47e10f0f5d5f8d922d5dc6bc5
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-22 16:31:17 +0200

    Minor changes

--- server/src/controllers/ai/hydrationPlan.ts
+++ server/src/controllers/ai/hydrationPlan.ts
@@ -22,15 +22,11 @@ export async function hydrationPlan(req: Request, res: Response) {
       );
     }
 
-    // 2️⃣ Fetch user profile from MongoDB
-    // const user = await User.findById(userId);
-    // if (!user) return res.status(404).json({ error: "User not found" });
+    // Fetch user profile from MongoDB
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ error: "User not found" });
 
-    const { weight, age, activityLevel } = {
-      weight: "87kg",
-      age: 22,
-      activityLevel: "Sedentary",
-    };
+    const { weight, age, activityLevel } = user;
 
     // 3️⃣ Get weather data based on longitude & latitude
     const weatherResponse = await axios.get(
